28bec60be4054491c0e23d47e0336ea7f220cf2d
Amarjeet <60272668+amarjeet-kaloty@users.noreply.github.com>
2020-11-06 22:49:16 -0500

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        max: 64
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('User', userSchema)
