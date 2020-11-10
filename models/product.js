568cc011417641a9aa0c7df4364055cc54a100dc
Amarjeet <60272668+amarjeet-kaloty@users.noreply.github.com>
2020-11-09 23:06:21 -0500

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+// Product Schema
+const productSchema = new mongoose.Schema({
+    product_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    manufacturer_name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    total_quantity: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Product', productSchema)
