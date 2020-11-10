29fce1597186b798507d6d8157f2f6ed44f79879
Amarjeet <60272668+amarjeet-kaloty@users.noreply.github.com>
2020-11-09 23:05:46 -0500

    Create order.js

---
+++ models/order.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const uuidv4 = requie('uuid/v4')
+const product = require('./product')
+
+// Order Schema
+const orderSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        default: uuidv4
+    },
+    orderData: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    order: {
+        type: [ product ],
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('order', orderSchema )
