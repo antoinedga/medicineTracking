fe13c0b08bb618efe476a86c56ea987e0a922008
Amarjeet <60272668+amarjeet-kaloty@users.noreply.github.com>
2021-01-30 12:05:11 -0500

    Update adminAuth.js

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -1,12 +1,19 @@
 const router = require('express').Router()
 
 //import admin controller
-const { admin_Login, admin_Registration } = require('../controllers/adminAuthController')
-const { invite } = require('../controllers/invite')
+const { get_admin_registration, post_admin_registration, get_admin_login, post_admin_Login, get_invitation, post_invitation  } = require('../controllers/adminAuthController')
+// const { invite } = require('../controllers/invite')
 
-router.post('/admin_registration', admin_Registration)
-router.post('/admin_login', admin_Login)
+// Get and Post for Admin Registration
+router.post('/admin_registration', get_admin_registration)
+router.post('/admin_registration', post_admin_registration)
 
-router.post('/admin_invite', invite)
+// Get and Post for Admin Login
+router.get('/admin_login', get_admin_login)
+router.post('/admin_login', post_admin_Login)
+
+// Get and Post for New User Invitations
+router.get('/admin_invite', get_invitation)
+router.post('/admin_invite', post_invitation)
 
 module.exports = router
