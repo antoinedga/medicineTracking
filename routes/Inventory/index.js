7c02d0e6c3d9807dcbf7df3147f1330f366ca610
tartuke <isaaclefler@gmail.com>
2021-02-27 18:42:48 -0500

    added Inventory and item base routes

--- routes/Inventory/index.js
+++ routes/Inventory/index.js
@@ -1,12 +1,25 @@
 var router = require('express').Router()
-var controller = require('../../controllers')
+var inventory = require('../../controllers/Inventory')
 
 
 module.exports = () => {
-router.get('/test',controller.inventory.test)
+router.get('/test',inventory.test)
+
 router.route('/')
-    .get(controller.inventory.getAll)
-    .post(controller.inventory.createInventory)
+    .get(inventory.getAll)
+    .post(inventory.create)
+
+router.route('/by_id')
+    .get(inventory.findByID)
+    .delete(inventory.deleteByID)
+
+router.route('/by_path')
+    .get(inventory.findByPath)
+    .delete(inventory.deleteByPath)
+
+router.route('/by_path/recursive')
+    .get(inventory.findRecursivelyByPath)
+    .delete(inventory.deleteRecursivelyByPath)
 
     return router
 }
