6dfdcb8037870567b89d488376226076c1cc0285
tartuke <isaaclefler@gmail.com>
2021-03-17 20:28:11 -0400

    conected order route

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,18 @@ const userRoute = require('./Authorization');
 const adminRoute = require('./Authorization/admin');
 const inventoryRoute = require('./Inventory');
 const roleRoute = require('./Role');
+const orderRoute = require('./order');
 
 
 module.exports = () => {
   router.use('/inventory', inventoryRoute());
   router.use('/user', userRoute());
-  router.use('/admin', adminRoute);
+  router.use('/admin', adminRoute());
   router.use('/role', roleRoute());
+  router.use('/order', orderRoute());
 
   router.get('*', (req, res) => {
-    res.status(404).json({ message: 'not a route' });
+    res.status(404).json({message: 'not a route'});
   });
 
   return router;
