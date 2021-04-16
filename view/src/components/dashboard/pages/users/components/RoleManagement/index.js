d6cd24cc88b94ffef435d81af57b13fcd9b01598
Antoine Gordon-Alvarez <antoinedga@knights.ucf.edu>
2021-04-15 22:44:09 -0400

    created view detail panel for role

--- view/src/components/dashboard/pages/users/components/RoleManagement/index.js
+++ view/src/components/dashboard/pages/users/components/RoleManagement/index.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Grid } from '@material-ui/core'
+import { Grid, Container, List, ListItemText, Divider } from '@material-ui/core'
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import icons from '../../../../../material-table-icon/index'
+import icons from '../../../../../../components/material-table-icon/index'
 import MaterialTable from 'material-table'
 import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux'
 import constants from '../../../../../../store/actions/actionType/admin'
 import { getAllRoles } from '../../../../../../store/actions/role.action'
 import DeleteDialog from './deleteDialogRole'
+import EditDialogRole from './editDialogRole'
 
 export default function RoleManagement(props) {
     const dispatch = useDispatch();
@@ -16,7 +17,10 @@ export default function RoleManagement(props) {
     const selected = useSelector(state => state.inventory.selected)
     const data = useSelector(state => state.admin.roles)
     const [deleteToggle, setDeleteView] = useState(false);
-    const [rowDataDelete, setRowData] = useState({})
+    const [rowDataDelete, setRowDataDelete] = useState({})
+
+    const [editToggle, setEditView] = useState(false);
+    const [rowDataEdit, setRowEdit] = useState({})
 
     useEffect(() => {
         console.log("ROLE is Loaded")
@@ -33,14 +37,23 @@ export default function RoleManagement(props) {
 
 
     const handleOpenDelete = (row) => {
-        setRowData(row)
+        setRowDataDelete(row)
         setDeleteView(true)
     }
 
     const handleDeleteClose = () => {
         setDeleteView(false)
-        setRowData({})
+        setRowDataDelete({})
+    }
 
+    const handleOpenEdit = (row) => {
+        setRowEdit(row)
+        setEditView(true)
+    }
+
+    const handleEditClose = () => {
+        setRowEdit({})
+        setEditView(false)
     }
 
     return (
@@ -67,7 +80,7 @@ export default function RoleManagement(props) {
                     {
                         icon: icons.Edit,
                         tooltip: 'Edit User',
-                        onClick: (event, rowData) => alert("You saved " + rowData.name)
+                        onClick: (event, rowData) => handleOpenEdit(rowData)
                     },
                     {
                         icon: icons.Delete,
@@ -85,17 +98,47 @@ export default function RoleManagement(props) {
                     pageSizeOptions: [10, 15, 20, 25, 50]
                 }}
             />
+            <EditDialogRole open={editToggle} handleClose={handleEditClose} rowData={rowDataEdit}></EditDialogRole>
             <DeleteDialog open={deleteToggle} handleClose={handleDeleteClose} rowData={rowDataDelete} />
         </div>
     )
 }
 
 function DetailPanel({ data }) {
-
+    console.log(data)
     return (
         <>
-            <div>
-                hello
+            <div style={{ height: '450px', overflow: 'auto' }}>
+                <Grid container>
+                    {
+                        data.permissions.map((element, index) => {
+                            return (
+                                <>
+                                    <Grid item xs={6} style={(index % 2 == 1) ? (
+                                        {
+                                            borderLeft: "1px solid",
+                                            borderColor: "rgba(0, 0, 0, 0.12)"
+                                        }
+                                    ) : null}>
+                                        <Container maxWidth="sm" style={{ paddingTop: '15px' }}>
+                                            <Typography variant="h5">Path: {element.path}</Typography>
+                                            <Typography variant="h6">Resource: {element.resource}</Typography>
+                                            <Divider variant={"middle"} />
+                                            <List>
+                                                {element.actions.map(element => {
+                                                    return (
+                                                        <ListItemText primary={element.action} secondary={`Recursive: ${element.recursive}`} />
+                                                    )
+                                                })}
+                                            </List>
+                                        </Container>
+
+                                    </Grid>
+                                </>)
+                        })
+                    }
+
+                </Grid>
             </div>
         </>
     )
